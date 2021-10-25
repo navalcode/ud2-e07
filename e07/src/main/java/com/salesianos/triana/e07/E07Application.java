85346ed03a4784e7fdc8cb5655a513b373b06c99
Manuel Naval <navalrod@gmail.com>
2021-10-25 09:49:22 +0200

    nuevo proyecto base

---
+++ e07/src/main/java/com/salesianos/triana/e07/E07Application.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.e07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E07Application.class, args);
+	}
+
+}
---
+++ e07/src/test/java/com/salesianos/triana/e07/E07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.e07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
